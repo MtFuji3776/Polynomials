f869a79332c3baea69596d613dab42192fa637df
mtfuji3776 <32.allegro.con.brio.ed.appassionato@gmail.com>
2021-08-29 01:37:44 +0900

    MapでPolynomialを定義してNumインスタンスにするところ

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,76 @@
 module Main where
 
 import Lib
+import qualified Data.Map as M
+import Data.Map(Map)
+import Data.Set(Set,singleton,toList,fromList)
+import qualified Data.Set as Set(map,empty)
+import Data.Maybe(fromMaybe)
+
 
 main :: IO ()
-main = someFunc
+main = 
+    let p = Poly $ M.fromList $ map (\u -> (varData u,u)) [Unit (M.fromList $ zip [1..] $ zipWith VD [1,2,3,4] [2,3,1,6]) 4]
+    in print $ p * p * p
+
+-- 変数毎に字数のデータを持たせておく
+data VarData = VD{var :: Int, degree :: Int} deriving(Eq,Ord)
+
+instance Show VarData where
+    show (VD v d) = "(X" ++ show v ++ " ^ " ++ show d ++ ")"
+
+-- 同じ変数同士の積で次数を変化させる関数
+multVarData :: VarData -> VarData -> Maybe VarData
+multVarData (VD v1 d1) (VD v2 d2) = 
+    if v1 == v2 
+        then Just $ VD v1 (d1 + d2)
+        else Nothing
+
+type Variables = Map Int VarData
+
+data Unit = Unit{
+                varData :: Variables,
+                coefficient :: Int
+            }deriving (Eq,Ord)
+
+addCoefficients :: Unit -> Unit -> Maybe Unit
+addCoefficients (Unit vd1 c1) (Unit vd2 c2) = 
+     if vd1 == vd2 
+        then Just $ Unit vd1 (c1 + c2)
+        else Nothing
+
+multipleUnits :: Unit -> Unit -> Unit
+multipleUnits (Unit vd1 c1) (Unit vd2 c2) =
+    let vd = M.unionWith (\x y -> fromMaybe (VD 0 0) $ multVarData x y) vd1 vd2
+        c  = c1 * c2
+    in Unit vd c
+
+instance Show Unit where
+    show (Unit vd c) = 
+        let vs = mconcat . map snd . M.toList $ fmap show vd
+        in show c ++ " * " ++ vs
+
+
+newtype Poly = Poly {unPoly :: Map Variables Unit} deriving(Eq,Ord,Show)
+
+multiplePoly :: Poly -> Poly -> Poly
+multiplePoly p1 p2 =
+    let l1 = map snd . M.toList . unPoly $ p1
+        l2 = map snd . M.toList . unPoly $ p2
+        l  = map (\u -> (varData u,u)) $ multipleUnits <$> l1 <*> l2
+        reduction :: [(Variables,Unit)] -> Poly -> Poly
+        reduction [] p     = p
+        reduction (x:xs) p = 
+            let vs = fst x
+                u  = snd x
+                addKeisuu x y = fromMaybe (Unit M.empty 0) $ addCoefficients x y
+                p' = Poly . M.insertWith addKeisuu vs u $ unPoly p
+            in reduction xs p'
+    in reduction l (Poly M.empty)
+
+instance Num Poly where
+    fromInteger n = Poly $ M.singleton  M.empty (Unit M.empty $ fromIntegral n) -- 定数項は0次元
+    p1 + p2 = Poly $ M.unionWith (\x y -> fromMaybe (Unit M.empty 0) $ addCoefficients x y) (unPoly p1) (unPoly p2)
+    p1 * p2 = multiplePoly p1 p2
+ 
+
--- app/Polynomials.hs
+++ app/Polynomials.hs
@@ -1,6 +1,7 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings,OverloadedLists #-}
 module Polynomials where
 
+import qualified Data.Map as M
 -- 1変数単項式
 data Uni = Uni {dim :: Int,comm :: Int}
 
@@ -9,14 +10,49 @@ instance Show Uni where
     show (Uni 1 c) = show c ++ "X"
     show (Uni n c) = show c ++ "X^" ++ show n
 
+instance Semigroup Uni where
+    u1 <> u2 = Uni (dim u1 + dim u2) (comm u1 * comm u2)
+
 instance Monoid Uni where
     mempty = Uni 0 1
-    mappend (Uni d1 c1) (Uni d2 c2) = Uni (d1 + d2) (c1 * c2)
+    mappend = (<>)
+
 
 -- instance IsString Uni where
 --     fromString xs = Uni 1 1 -- あまり筋はよくない。多変数の時にこそ意味を持つ気がする。
 
-data Poly = One Uni | Add Poly Poly | Mul Poly Poly
+
+data Poly = One Uni | Add Poly Poly | Mul Poly Poly 
+-- 文法的にはOneとAddだけで任意の多項式を構成できるが、多項式環としての構造を与えたいのでMulを付与
+    -- 多項式の標準形とはこの場合、Mulを含まない式のことと定義できそうだ。
+newtype Polyno = P{ unP :: M.Map Int Uni}
+
+instance Show Polyno where
+    show (P m) =
+        let xs = map snd $ M.toList m
+        in concatMap show xs
+
+instance Num Polyno where
+    fromInteger n = P $ M.singleton 0 (Uni 0 (fromInteger n))
+    p1 + p2 = 
+        let p1' = unP p1
+            p2' = unP p2
+        in P $ M.unionWith (\u1 u2 -> u2{comm = comm u2 + comm u1}) p1' p2'
+    p1 * p2 = 
+        let p1' = unP p1
+            p2' = unP p2
+            xs = map snd $ M.toList (unP p1)
+        in P $ foldr (\u p -> fmap (u <>) p) p2' xs
+    abs p = let p' = unP p in P $ fmap (\u -> u{comm = abs $ comm u}) p'
+    negate p = let p' = unP p in P $ fmap (\u -> u{comm = negate $ comm u}) p'
+    signum p = 
+        let p' = unP p
+            m = fst $ M.findMax p'
+            u = snd $ M.findMax p'
+        in P $ M.singleton m $ Uni m (signum $ comm u)
+
+
+
 
 instance Show Poly where
     show (One u) = show u
@@ -43,3 +79,4 @@ eval :: Poly -> Int -> Int
 eval (One (Uni d c)) k = c * product (replicate d k)
 eval (Add p1 p2) k = eval p1 k + eval p2 k
 eval (Mul p1 p2) k = eval p1 k * eval p2 k
+
